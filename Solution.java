fd546d5139ce6f51c64d005ac55263b244a9d0b3
dumisani <chmdum003@myuct.ac.za>
2025-11-01 23:25:20 +0200

    Used the two pointer approach to check if numbers are consecutive, the numbers are grouped into a range if they are consecutive and they are not just add them to the output string
    #To-do: remove the last comma

--- Solution.java
+++ Solution.java
@@ -6,7 +6,7 @@ import java.util.Collection;
 public class Solution implements NumberRangeSummarizer{
     public static void main(String[] args) {
         Solution sl = new Solution();
-        Collection<Integer> input = sl.collect("1,3,6,7,8,12,13,14,15,21,22,23,24,31"); // assumption no1, the input is a sorted string of numbers
+        Collection<Integer> input = sl.collect("1,3,4,5,7,9,10,11,13,26,27"); // assumption no1, the input is a sorted string of numbers
         System.out.println(sl.summarizeCollection(input));
 
     }
@@ -25,13 +25,42 @@ public class Solution implements NumberRangeSummarizer{
 
     @Override
     public String summarizeCollection(Collection<Integer> input) {
-        ArrayList<Integer> inputArrayList = new ArrayList<>(input);
-        StringBuilder sb = new StringBuilder();
-        for (int i = 1; i < input.size(); i++) {
-            if (inputArrayList.get(i) - inputArrayList.get(i-1) != 1) {
-                sb.append(inputArrayList.get(i-1) + ",");
+        ArrayList<Integer> inputArrayList = new ArrayList<>(input); // change the collection into an arraylist for access using indexes
+        StringBuilder sb = new StringBuilder(); // build the output string
+        String group = ""; // to temporarily store the range if it does exist
+        // pointers to check for consecutive numbers
+        int pointer1 = 0;
+        int pointer2 = 1;
+
+        while(pointer2 < inputArrayList.size()){
+
+            if(inputArrayList.get(pointer2)-inputArrayList.get(pointer1)!=1){ // if the numbers are not consecutive
+                sb.append(inputArrayList.get(pointer1)+", ");
+                pointer1++;
+                pointer2++;
+            }
+            else{ // this is for when they are consecutive
+                group+= inputArrayList.get(pointer1)+"-";
+                // the pointers continue moving as long as the numbers are consecutive
+                while(pointer2<inputArrayList.size()&&inputArrayList.get(pointer2)-inputArrayList.get(pointer1)==1){
+                    pointer1++;
+                    pointer2++;
+                }
+                // add the last number in the pair
+                group+= inputArrayList.get(pointer1);
+                sb.append(group+", "); // add the group to the output string
+                // reset the pointers and the group string that temporarily stores the group
+                pointer1=pointer2;
+                pointer2++;
+                group="";
             }
         }
+
+        // if the is an element left just add it to the end
+        if (pointer1 < inputArrayList.size()) {
+            sb.append(inputArrayList.get(pointer1));
+        }
+
         return sb.toString();
     }
 }
