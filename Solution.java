f54aceb3e5396317db4c6d218dc77b3f7f231a2d
dumisani <chmdum003@myuct.ac.za>
2025-11-01 03:16:15 +0200

    added a solution class that implements the NumberRangeSummarizer and it's methods. The collect method now returns an arraylist with numbers from the input string

---
+++ Solution.java
@@ -0,0 +1,35 @@
+package numberrangesummarizer;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Solution implements NumberRangeSummarizer{
+    public static void main(String[] args) {
+        Solution sl = new Solution();
+        sl.collect("1,3,6,7,8,12,13,14,15,21,22,23,24,31"); // assumption no1, the input is a sorted string of numbers
+    }
+
+    @Override
+    public Collection<Integer> collect(String input) {
+
+        String[] inputIntegers = input.split(","); // split the string by commas
+        ArrayList<Integer> inputArrayList = new ArrayList<>();
+
+        for (String no : inputIntegers) {
+            // System.out.println(no);
+            inputArrayList.add(Integer.parseInt(no)); // add the numbers from the split array to the inputArrayList
+        }
+        // for (Integer integer : inputArrayList) {
+        //     System.out.print(integer+", ");
+        // }
+        return inputArrayList;
+    }
+
+    @Override
+    public String summarizeCollection(Collection<Integer> input) {
+
+        StringBuilder sb = new StringBuilder();
+        
+        return sb.toString();
+    }
+}
