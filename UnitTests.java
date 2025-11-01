0b1e0c0d0e3f985bd754fb45ee6324bb1d37c363
dumisani <chmdum003@myuct.ac.za>
2025-11-01 03:41:15 +0200

    added a new test for the collect method

--- UnitTests.java
+++ UnitTests.java
@@ -2,6 +2,14 @@ package numberrangesummarizer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 public class UnitTests {
-    
+    @Test
+    public void testCollect(){
+        Solution s = new Solution();
+        Collection<Integer> list = s.collect("1,2,3,4,5,6,7,8,9,10");
+        Assertions.assertEquals(Arrays.asList(1,2,3,4,5,6,7,8,9,10), list);
+    }
 }
