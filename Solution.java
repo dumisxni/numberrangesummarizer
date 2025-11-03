3de1a31d7463a84c8474d824fd049d001e8d486e
dumisani <chmdum003@myuct.ac.za>
2025-11-03 02:55:22 +0200

    1. Added a catch statement for invalid input 2. Ensured that the input is accepted even if there are spaces in between the numbers 3. Checked for valid input, handled null and empty input 4. Added a check for a collection containing null values

--- Solution.java
+++ Solution.java
@@ -6,25 +6,39 @@ import java.util.Collection;
 public class Solution implements NumberRangeSummarizer{
     public static void main(String[] args) {
         Solution sl = new Solution();
-        Collection<Integer> input = sl.collect("1,3,4,5,7,9,10,11,13,26,27"); // assumption no1, the input is a sorted string of numbers
+        Collection<Integer> input = sl.collect("-3, -2, -1, 0, 3, 4"); // assumption no1, the input is a sorted string of numbers
         System.out.println(sl.summarizeCollection(input));
 
     }
 
     @Override
     public Collection<Integer> collect(String input) {
-
-        String[] inputIntegers = input.split(","); // split the string by commas
         ArrayList<Integer> inputArrayList = new ArrayList<>();
+        if(input == null || input.isEmpty() ) {
+            System.out.println("Input String can't be null or empty");
+            return inputArrayList;
+        }
+        String[] inputIntegers = input.split(","); // split the string by commas
 
         for (String no : inputIntegers) {
-            inputArrayList.add(Integer.parseInt(no)); // add the numbers from the split array to the inputArrayList
+            try {
+                inputArrayList.add(Integer.parseInt(no.trim())); // add the numbers from the split array to the inputArrayList
+            }
+            catch (NumberFormatException e) {
+                System.out.println("Input String is not a number");
+                inputArrayList.clear();
+                return inputArrayList;
+            }
         }
         return inputArrayList;
     }
 
     @Override
     public String summarizeCollection(Collection<Integer> input) {
+
+        if(input == null || input.isEmpty() || input.contains(null)) {
+            return "Input Collection can't be null, contain null elements or empty";
+        }
         ArrayList<Integer> inputArrayList = new ArrayList<>(input); // change the collection into an arraylist for access using indexes
         StringBuilder sb = new StringBuilder(); // build the output string
         String group = ""; // to temporarily store the range if it does exist
@@ -61,6 +75,6 @@ public class Solution implements NumberRangeSummarizer{
             sb.append(inputArrayList.get(pointer1));
         }
 
-        return sb.toString().replaceAll(", $", ""); // remove the extraspaces or commas at the end
+        return sb.toString().replaceAll(", $", ""); // remove the extra spaces or commas at the end
     }
 }
